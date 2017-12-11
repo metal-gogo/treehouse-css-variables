ebc76bf149475933511aa2cbc8fd589c0024b5b3
Daniel González <dgonzalezg@gbm.com.mx>
2017-12-11 12:02:09 -0600

    :zap: added presentation cover

--- css-variables-presentation/src/presentation.js
+++ css-variables-presentation/src/presentation.js
@@ -37,58 +37,26 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
+        progress={'bar'}
         transition={['zoom', 'slide']}
         transitionDuration={500}
         theme={theme}
       >
         <Slide transition={['zoom']} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            CSS Variables
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Heading margin="10px 0 0" size={2} textColor="tertiary" fit bold>
+            why would I need them if I use [insert favourite CSS preprocessor
+            name]?
+          </Heading>
+          <Text margin="100px 0 0" textColor="secondary">
+            by Daniel González
           </Text>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary" />
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary" />
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary" />
       </Deck>
     );
   }
